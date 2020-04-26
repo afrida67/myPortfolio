9da8a9833fce3b282d030c2c5624a80396477247
afrida67 <afrida660@gmail.com>
2020-04-27 01:03:25 +0600

    Updates

---
+++ precache-manifest.0d053c763c3582600a492a8c19727391.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a388191cf9fcab71dd2126424da745",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "b2d55459a74bc70d49b4",
+    "url": "/myPortfolio/static/css/2.57ad4adf.chunk.css"
+  },
+  {
+    "revision": "966c2db6037b9aa5e5b5",
+    "url": "/myPortfolio/static/css/main.a60a9aa9.chunk.css"
+  },
+  {
+    "revision": "b2d55459a74bc70d49b4",
+    "url": "/myPortfolio/static/js/2.f6b67070.chunk.js"
+  },
+  {
+    "revision": "966c2db6037b9aa5e5b5",
+    "url": "/myPortfolio/static/js/main.2a40b26f.chunk.js"
+  },
+  {
+    "revision": "2261391d3370eb3cf5eb",
+    "url": "/myPortfolio/static/js/runtime~main.918aa22c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/myPortfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/myPortfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/myPortfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/myPortfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/myPortfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
